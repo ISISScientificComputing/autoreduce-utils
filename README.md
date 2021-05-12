a43511e38a0b38720efb98704e3fa3fa3fe2c1b3
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-12 14:58:17 +0100

    Add make, README and update setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This repository contains the utils that are shared across the backend and the frontend.
+This package contains the utils that are shared across the backend and the frontend of Autoreduce.
+This was previously the `utils` folder.
--- setup.py
+++ setup.py
@@ -5,15 +5,21 @@ see:
     `python setup.py help`
 for more details
 """
+from os import path
 from setuptools import setup, find_packages
 
+# read the contents of the README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='autoreduce_utils',
-      version='1.0.0',
-      description='ISIS Autoreduce ',
+      version='0.1.1',
+      description='ISIS Autoreduce',
       author='ISIS Autoreduction Team',
       url='https://github.com/ISISScientificComputing/autoreduce-utils/',
       install_requires=[
-          'Django==3.2',
+          'Django==3.2.2',
           'gitpython==3.1.14',
           'python-icat==0.18.1',
           'suds-py3==1.4.4.1',
@@ -21,4 +27,6 @@ setup(name='autoreduce_utils',
       ],
       packages=find_packages(),
       package_data={"autoreduce_utils": ["test_credentials.ini"]},
-      entry_points={"console_scripts": ["autoreduce-creds-migrate = autoreduce_utils.migrate_credentials:main"]})
+      entry_points={"console_scripts": ["autoreduce-creds-migrate = autoreduce_utils.migrate_credentials:main"]},
+      long_description=long_description,
+      long_description_content_type='text/markdown')
