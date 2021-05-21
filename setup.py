394fa2ca3c190efb2c8868f10b6d7ac6a0de82f5
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-05-21 09:59:08 +0100

    Update version to 0.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='autoreduce_utils',
-      version='0.1.2',
+      version='0.1.3',
       description='ISIS Autoreduce',
       author='ISIS Autoreduction Team',
       url='https://github.com/ISISScientificComputing/autoreduce-utils/',
