8c20e65521ddb68b8e5c9e9218df702fde771c76
Dagonite <up821535@myport.ac.uk>
2021-10-07 13:28:35 +0000

    Add run_title to Message class

--- autoreduce_utils/message/message.py
+++ autoreduce_utils/message/message.py
@@ -4,9 +4,7 @@
 # Copyright &copy; 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 # ############################################################################### #
-"""
-Represents the messages passed between AMQ queues
-"""
+"""Represents the messages passed between AMQ queues."""
 import json
 
 import attr
@@ -17,12 +15,13 @@ from autoreduce_utils.message.validation import stages
 @attr.s
 class Message:
     """
-    A class that represents an AMQ Message.
-    Messages can be serialized and deserialized for sending messages to and from AMQ
+    A class that represents an AMQ Message. Messages can be serialized and
+    deserialized for sending messages to and from AMQ.
     """
     description = attr.ib(default="")
     facility = attr.ib(default="ISIS")
     run_number = attr.ib(default=None)
+    run_title = attr.ib(default=None)
     instrument = attr.ib(default=None)
     rb_number = attr.ib(default=None)
     started_by = attr.ib(default=None)
@@ -42,10 +41,16 @@ class Message:
 
     def serialize(self, indent=None, limit_reduction_script=False):
         """
-        Serialized member variables as a json dump
-        :param indent: The indent level passed to json.dumps
-        :param limit_reduction_script: if True, limits reduction_script to 50 chars in return
-        :return: JSON dump of a dictionary representing the member variables
+        Serialized member variables as a JSON dump.
+
+        Args:
+            indent: The indent level passed to `json.dumps`.
+
+            limit_reduction_script: If True, limit reduction_script to 50 chars
+            in return.
+
+        Returns:
+            JSON dump of a dictionary representing the member variables.
         """
         data_dict = attr.asdict(self)
         if limit_reduction_script:
@@ -56,18 +61,31 @@ class Message:
     @staticmethod
     def deserialize(serialized_object):
         """
-        Deserialize an object and return a dictionary of that object
-        :param serialized_object: The object to deserialize
-        :return: Dictionary of deserialized object
+        Deserialize an object and return a dictionary of that object.
+
+        Args:
+            serialized_object: The object to deserialize.
+
+        Returns:
+            Dictionary of deserialized object.
         """
         return json.loads(serialized_object)
 
     def populate(self, source, overwrite=True):
         """
-        Populate the class from either a serialised object or a dictionary optionally retaining
-        or overwriting existing values of attributes
-        :param source: Object to populate class from
-        :param overwrite: If True, overwrite existing values of attributes
+        Populate the class from either a serialised object or a dictionary
+        optionally retaining or overwriting existing values of attributes.
+
+        Args:
+            source: Object to populate class from.
+
+            overwrite: If True, overwrite existing values of attributes.
+
+        Raises:
+            ValueError: If unable to recognise the serialised object.
+
+            ValueError: If an unexpected key is encountered during message
+            population.
         """
         if isinstance(source, str):
             try:
@@ -80,21 +98,23 @@ class Message:
             if key in self_dict.keys():
                 self_value = self_dict[key]
                 if overwrite or self_value is None:
-                    # Set the value of the variable on this object accessing it by name
+                    # Set the value of the variable on this object accessing it
+                    # by name
                     setattr(self, key, value)
             else:
                 raise ValueError(f"Unexpected key encountered during Message population: '{key}'.")
 
-    def validate(self, destination):
+    def validate(self, destination: str):
         """
-        Ensure that the message is valid to be sent to a given destination queue
-        :param destination: (str) The name of the queue to send the data to
+        Ensure that the message is valid to be sent to a given destination
+        queue.
+
+        Args:
+            destination: The name of the queue to send the data to
         """
         if destination == '/queue/DataReady':
             stages.validate_data_ready(self)
 
     def to_dict(self):
-        """
-        Convert the message to a Python dictionary
-        """
+        """Return the message as a Python dictionary."""
         return attr.asdict(self)
