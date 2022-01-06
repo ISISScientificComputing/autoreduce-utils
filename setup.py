844a127c852974348179490aedd87676c49eff86
James Brookes <james.brookes@stfc.ac.uk>
2022-01-06 13:49:48 +0000

    increment gitpython

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url='https://github.com/autoreduction/autoreduce-utils/',
     install_requires=[
         'attrs==21.2.0',
-        'gitpython==3.1.14',
+        'gitpython==3.1.20',
         'python-icat==0.20.1',
         'suds-py3==1.4.5.0',
         'stomp.py==7.0.0',
