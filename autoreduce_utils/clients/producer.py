b2734bd5f3201f4129fa0ae75d30ebe210af6a5c
James Brookes <james.brookes@stfc.ac.uk>
2022-02-11 04:06:07 +0000

    Switch Publisher to Threads. Add functions to start a thread and run indefinitely

--- autoreduce_utils/clients/producer.py
+++ autoreduce_utils/clients/producer.py
@@ -1,42 +1,91 @@
 import json
 import os
+import threading
 import time
 import logging
+import traceback
 
 from kafka import KafkaProducer
 from kafka.errors import NoBrokersAvailable
+from autoreduce_utils.clients.connection_exception import ConnectionException
 
 from autoreduce_utils.message.message import Message
 
-TRANSACTIONS_TOPIC = os.environ.get('TRANSACTIONS_TOPIC')  #"/queue/DataReady"
+TRANSACTIONS_TOPIC = os.environ.get('KAFKA_TOPIC')  #"data_ready"
 KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
 
 
-class Publisher:
+class Publisher(threading.Thread):
     """
     A class to publish messages to a Kafka topic
     """
 
     def __init__(self):
+        super().__init__()
         self.logger = logging.getLogger(__package__)
+        self.logger.debug("Initializing the producer")
         self.producer = None
+        self._stop_event = threading.Event()
 
-        if self.producer is None:
+        while self.producer is None:
             try:
-                self.producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL)
+                self.logger.debug("Getting the kafka producer")
+                self.producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER_URL,
+                                              value_serializer=lambda v: json.dumps(v).encode('utf-8'))
             except NoBrokersAvailable as err:
-                self.logger.error("Unable to find a broker: %s".format(err))
+                self.logger.error("Unable to find a broker: %s", (err))
                 time.sleep(1)
 
-    def push(self, message):
-        self.logger.debug("Publishing: %s".format(message))
+    def run(self):
+        """
+        Run the thread
+        """
+        self.logger.debug("Running the thread")
+        while not self._stop_event.is_set():
+            time.sleep(1)
+            if self._stop_event.is_set():
+                break
+        self.producer.close()
+
+    def send(self, message):
+        """ Publish a message to the topic """
+        self.logger.debug("Publishing: %s", (message))
         try:
             if self.producer:
                 if isinstance(message, Message):
                     message_json_dump = message.serialize()
                 else:
                     message_json_dump = message
-                ack = self.producer.send(TRANSACTIONS_TOPIC, message_json_dump)
-                metadata = ack.get()
+                self.producer.send(TRANSACTIONS_TOPIC, message_json_dump)
         except AttributeError:
-            self.logger.error("Unable to send %s. The producer does not exist.".format(message))
+            self.logger.error("Unable to send %s. The producer does not exist.", (message))
+
+
+def setup_connection() -> Publisher:
+    """
+    Starts the Kafka producer.
+    """
+
+    publisher = Publisher()
+
+    t1 = threading.Thread(target=publisher.run)
+    t1.start()
+
+    return publisher
+
+
+def main():
+    """Entry point for the module."""
+    logger = logging.getLogger(__package__)
+    try:
+        setup_connection()
+    except ConnectionException as exp:
+        logger.error("Exception occurred while connecting: %s %s\n\n%s",
+                     type(exp).__name__, exp, traceback.format_exc())
+        raise
+
+    logger.info("Kafka producer started.")
+
+
+if __name__ == '__main__':
+    main()
