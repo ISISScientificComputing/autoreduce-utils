c3a0b37ba15d1a0ef587046f03b253c0d7cac6bb
James Brookes <james.brookes@stfc.ac.uk>
2022-02-15 15:05:01 +0000

    New approach to creating Producer without using threads

---
+++ autoreduce_utils/clients/producer.py
@@ -0,0 +1,69 @@
+import logging
+import os
+
+import confluent_kafka
+
+log = logging.getLogger(__name__)
+
+
+class Publisher(object):
+    """Wrapper around asynchronous Kafka Producer"""
+
+    def __init__(self, **config):
+        """
+        Create new Producer wrapper instance.
+
+        :param str bootstrap_servers: Initial list of brokers as a CSV
+        list of broker host or host:port.
+        :param config Configuration properties
+        """
+
+        config['bootstrap.servers'] = os.environ.get('KAFKA_BROKER_URL')
+        self._producer = confluent_kafka.Producer(config)
+
+    @staticmethod
+    def delivery_report(err, msg):
+        """
+        Callback called once for each produced message to indicate the final
+        delivery result. Triggered by poll() or flush().
+
+        :param confluent_kafka.KafkaError err: Information about any error
+        that occurred whilst producing the message.
+        :param confluent_kafka.Message msg: Information about the message
+        produced.
+        :returns: None
+        :raises confluent_kafka.KafkaException
+        """
+
+        if err is not None:
+            log.exception('Message delivery failed: {}'.format(err))
+            raise confluent_kafka.KafkaException(err)
+        else:
+            log.debug('Message delivered to {} [{}]: {}'.format(msg.topic(), msg.partition(), msg.value()))
+
+    def publish(self, topic, messages, key=None, timeout=2):
+        """
+        Publish messages to the topic.
+
+        :param str topic: Topic to produce messages to.
+        :param list(str) messages:  List of message payloads.
+        :param str key: Message key.
+        :param float timeout: Maximum time to block in seconds.
+        :returns: Number of messages still in queue.
+        :rtype int
+        """
+
+        if not isinstance(messages, list):
+            messages = [messages]
+
+        try:
+            for m in messages:
+                record_value = m.json()
+                self._producer.produce(topic, record_value, key, callback=Publisher.delivery_report)
+                self._producer.poll(0)
+
+            return self._producer.flush(timeout)
+
+        except (BufferError, confluent_kafka.KafkaException, NotImplementedError):
+            log.exception('Error publishing to {} topic.'.format(topic))
+            raise
---
+++ autoreduce_utils/clients/tests/test_producer.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from unittest import TestCase, mock
+import confluent_kafka
+from autoreduce_utils.clients import producer
+from autoreduce_utils.message.message import Message
+
+FAKE_KAFKA_TOPIC = 'topic'
+FAKE_KAFKA_URL = 'FAKE_KAFKA_URL'
+
+
+class TestConfluentKafkaProducer(TestCase):
+
+    @mock.patch('confluent_kafka.Producer')
+    @mock.patch.dict(os.environ, {"KAFKA_BROKER_URL": "FAKE_KAFKA_URL"}, clear=True)
+    def setUp(self, mock_confluent_producer):
+        super(TestConfluentKafkaProducer, self).setUp()
+        self.mock_confluent_producer = mock_confluent_producer
+        self.prod = producer.Publisher()
+
+    def tearDown(self):
+        super(TestConfluentKafkaProducer, self).tearDown()
+
+    def test_kafka_producer_init(self):
+        expected_config = {'bootstrap.servers': FAKE_KAFKA_URL}
+
+        self.mock_confluent_producer.assert_called_once_with(expected_config)
+        self.assertEqual(self.mock_confluent_producer.return_value, self.prod._producer)
+
+    def test_kafka_producer_publish(self):
+        topic = FAKE_KAFKA_TOPIC
+        test_message = Message()
+        messages = [test_message]
+        expected_message = test_message.json()
+
+        self.prod.publish(topic, messages)
+
+        produce_callback = producer.Publisher.delivery_report
+        self.prod._producer.produce.assert_called_once_with(topic, expected_message, None, callback=produce_callback)
+        self.prod._producer.flush.assert_called_once()
+
+    def test_kafka_producer_publish_one_message_with_key(self):
+        topic = FAKE_KAFKA_TOPIC
+        one_message = Message()
+        key = '1000'
+        expected_message = one_message.json()
+
+        self.prod.publish(topic, one_message, key)
+
+        produce_callback = producer.Publisher.delivery_report
+        self.prod._producer.produce.assert_called_once_with(topic, expected_message, key, callback=produce_callback)
+        self.prod._producer.flush.assert_called_once()
+
+    def test_kafka_producer_publish_exception(self):
+        topic = FAKE_KAFKA_TOPIC
+        test_message = Message()
+        messages = [test_message]
+        self.prod._producer.produce.side_effect = \
+            confluent_kafka.KafkaException
+
+        self.assertRaises(confluent_kafka.KafkaException, self.prod.publish, topic, messages)
+
+    @mock.patch('confluent_kafka.Message')
+    def test_delivery_report_exception(self, mock_message):
+        self.assertRaises(confluent_kafka.KafkaException, self.prod.delivery_report, confluent_kafka.KafkaError,
+                          confluent_kafka.Message)
+
+    @mock.patch('confluent_kafka.Message')
+    def test_delivery_report(self, mock_message):
+        self.prod.delivery_report(None, confluent_kafka.Message)
+
+    @mock.patch('confluent_kafka.Message')
+    def test_delivery_report_with_unicode(self, mock_message):
+        mock_message.topic.return_value = 'test_topic'
+        mock_message.partition.return_value = '1'
+        mock_message.value.return_value = 'gęś'
+        self.prod.delivery_report(None, mock_message)
